77ed019186304a96b8ee5a47dfc771bdf7677448
nico-stylemans <58655067+nico-stylemans@users.noreply.github.com>
2023-01-06 15:32:54 +0100

    Create WafPolicyDeploy.tf

---
+++ examples/WafPolicy/WafPolicyDeploy.tf
@@ -0,0 +1,41 @@
+# Define Terraform provider
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+# Configure the Azure provider
+provider "azurerm" {
+  subscription_id = var.subid
+  client_id       = var.clientid
+  client_secret   = var.client_secret
+  tenant_id       = var.tenantid
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+module "Resource-Group" {
+  source         = "./modules/azure/ResourceGroup"
+
+  rg             = var.rg
+  location       = var.location          
+}
+
+module "WafPolicies" {
+  source         = "./Modules/Azure/WafPolicy"
+
+  resource_group = var.rg
+  location       = var.location
+  wafpolicylist = var.wafpolicylist   
+  
+  depends_on = [module.Resource-Group.rg-info]
+}
