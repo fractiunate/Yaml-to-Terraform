f9d1794350bc56a7eba34cd51f97ece1eaa9b227
Fractiunate// David Rahäuser <78024279+fractiunate@users.noreply.github.com>
2024-06-21 12:29:42 +0200

    Update TerraYaml.py

--- Python/yamltovars/TerraYaml.py
+++ Python/yamltovars/TerraYaml.py
@@ -9,34 +9,51 @@ def parse_yaml(yaml, indent):
     terrafile = ""
     line = ""
     
-    for k, v in yaml.items():
-        
-        if isinstance(v, str):
+    for k, v in yaml.items():        
+        if isinstance(v, bool):
+            line = k + " = " + str(v).lower() + "\n"
+        elif isinstance(v, str):
             line = k + " = " + '"' + v + '"' + "\n"
-        if isinstance(v, dict):
+        elif isinstance(v, int):
+            line = k + " = " + v + "\n"
+        elif isinstance(v, dict):
             line = parse_dict(v, k, indent, indent)
-        if isinstance(v, list):
+        elif isinstance(v, list):
             line = parse_list(v, k, indent, indent)
-        terrafile = terrafile + line + "\n"
+        # elif isinstance(v, None): TODO: implment? --> currently is None instead of empty string
+        else:
+            print(f'[WARN] Unkown type for {k} {v}')
+            line = ""
+
+        if line != "":
+            terrafile = terrafile + line + "\n" 
 
     return terrafile
 
 def parse_list(mylist, listname, indent, currentident):
     
-    endlistitem = "}\n"
+    endlistitem = "},\n"
 
     if all(isinstance(item, str) for item in mylist):
-
         line = listname + " = " + str(mylist).replace("'", '"') + "\n"
-        line = line.rjust(len(line)+ currentident)
-        
+        line = line.rjust(len(line))
+        return line
+    # TODO: int
+    elif all(isinstance(item, int) for item in mylist):
+        line = listname + " = " + str(mylist).replace("'", '"') + "\n"
+        line = line.rjust(len(line))
+        return line
+    elif all(isinstance(item, bool) for item in mylist):
+        line = listname + " = " + str(mylist).replace("'", '').lower() + "\n"
+        line = line.rjust(len(line))
         return line
 
-    line = listname + " = {\n"
+    line = listname + " = [\n"
+
     
     for count, lijstitem in enumerate(mylist):
         
-        listitem = listname + str(count) + " = {\n"
+        listitem = "{\n"
         line = line + listitem.rjust(len(listitem) + currentident)
         
         if isinstance(lijstitem, dict):
@@ -48,7 +65,7 @@ def parse_list(mylist, listname, indent, currentident):
 
         line = line + endlistitem.rjust(len(endlistitem) + currentident)
 
-    return line + endlistitem.rjust(len(endlistitem) + (currentident - indent))
+    return line + "]\n".rjust(len(endlistitem) + (currentident - indent) -1)
 
 def parse_dict(mydict, dictname, indent, currentident):
     
@@ -59,13 +76,18 @@ def parse_dict(mydict, dictname, indent, currentident):
     else:
         line = (dictname + " = {\n")
     for label, value in mydict.items():
-
         if isinstance(value, list):
             rcline = parse_list(value, label, indent, currentident + indent)
             line = line + rcline.rjust(len(rcline) + currentident)
         elif isinstance(value, dict):
             rcline = parse_dict(value, label, indent, currentident + indent)
             line = line + rcline.rjust(len(rcline) + currentident)
+        elif isinstance(value, bool):
+            dictitem = label + " = " + str(value).lower() + "\n"
+            line = line + dictitem.rjust(len(dictitem) + currentident)
+        elif isinstance(value, int):
+            dictitem = label + " = " + str(value) + "\n"
+            line = line + dictitem.rjust(len(dictitem) + currentident)
         else:
             dictitem = label + " = " + '"' + str(value) + '"' + "\n"
             line = line + dictitem.rjust(len(dictitem) + currentident)
